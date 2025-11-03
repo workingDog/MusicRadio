52445d0e3d888d2d19d62143ef2ca93e4316e627
workingdog <ringowathelet@gmail.com>
2025-11-03 19:46:26 +0900

    initial commit

--- MusicRadio/Views/CountriesView.swift
+++ MusicRadio/Views/CountriesView.swift
@@ -11,6 +11,9 @@ import SwiftData
 struct CountriesView: View {
     @Query(sort: [SortDescriptor(\Country.name, order: .forward)])
     private var countries: [Country]
+    
+    @State private var searchText = ""
+    @State private var filteredCountries: [Country] = []
 
     var body: some View {
         NavigationStack {
@@ -21,16 +24,34 @@ struct CountriesView: View {
                     endPoint: .bottomTrailing
                 )
                 .ignoresSafeArea()
-                List(countries) { country in
+                List(filteredCountries) { country in
                     NavigationLink(destination: CountryView(country: country)) {
                         Text(country.name).bold()
                     }
                     .listRowBackground(Color.clear)
+                    .searchable(text: $searchText, prompt: "Search countries")
+                    .onSubmit(of: .search) {
+                        applySearch()
+                    }
                 }
                 .scrollContentBackground(.hidden)
             }
             .navigationBarTitle("Countries")
         }
+        .onAppear{
+            filteredCountries = countries
+        }
     }
-
+    
+    private func applySearch() {
+        let trimmed = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
+        if trimmed.isEmpty {
+            filteredCountries = countries
+        } else {
+            filteredCountries = countries.filter {
+                $0.name.lowercased().starts(with: searchText.lowercased())
+            }
+        }
+    }
+    
 }
